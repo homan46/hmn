889e725e89500248cb59db92dcba35d994df7ac0
mann <fake.fake.com>
2022-05-01 17:08:12 +0800

    support https

--- config/model.go
+++ config/model.go
@@ -5,4 +5,11 @@ type Config struct {
 		Type string `json:"type"`
 		Path string `json:"path"`
 	} `json:"storage"`
+	Server struct {
+		UseHttps     bool     `json:"use_https"`
+		TlsKey       string   `json:"tls_key"`
+		TlsCert      string   `json:"tls_cert"`
+		ListenOn     string   `json:"listen_on"`
+		AllowOrigins []string `json:"allow_origins"`
+	} `json:"server"`
 }
--- main.go
+++ main.go
@@ -18,6 +18,12 @@ func main() {
 	conf := config.LoadConfig("./config.json")
 	db := data.OpenDB(conf)
 	business := business.NewBusunessLayer(db)
-	server := web.New(business)
-	server.Logger.Fatal(server.Start(":8080"))
+	server := web.New(business, conf)
+
+	if conf.Server.UseHttps {
+		server.Logger.Fatal(server.StartTLS(conf.Server.ListenOn, conf.Server.TlsCert, conf.Server.TlsKey))
+	} else {
+		server.Logger.Fatal(server.Start(conf.Server.ListenOn))
+	}
+
 }
